cd098272030279eabaea95a14e472b1702c98d50
Aurélien <aurelien.strimelle@bstorm.be>
2024-10-17 11:34:59 +0200

    Add product type 🧈

---
+++ src/@types/product.d.ts
@@ -0,0 +1,11 @@
+export type Product = {
+  id: string;
+  name: string;
+  quantity: number;
+  price: number;
+  isActive: boolean;
+  createAt: string; 
+  updateAt: string;
+}
+
+export type ProductForm = Omit<Product, 'id' | 'isActive' | 'createAt' | 'updateAt'>;
